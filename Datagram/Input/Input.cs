b4f13751dbb3604a00f03d16f2bd5fa21f9ce493
高东霞 <dongxia.gao@dazong.com>
2016-09-21 17:48:33 +0800

    1、数据报模式C-S通信

---
+++ Datagram/Input/Input.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+namespace WCF.Second
+{
+    class Input
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //建立和发送端相同的通道栈
+                BindingElement[] bindingElements = new BindingElement[3];
+                bindingElements[0] = new TextMessageEncodingBindingElement();
+                bindingElements[1] = new OneWayBindingElement();
+                bindingElements[2] = new HttpTransportBindingElement();
+                CustomBinding binding = new CustomBinding(bindingElements);
+                //建立ChannelListner
+                IChannelListener<IInputChannel> listener = binding.BuildChannelListener<IInputChannel>(new Uri("http://localhost/InputService"), new BindingParameterCollection());
+                listener.Open();
+                //创建IInputChannel
+                IInputChannel inputChannel = listener.AcceptChannel();
+                inputChannel.Open();
+                Console.WriteLine("开始接受消息。。。");
+                //接受并打印消息
+                Message message = inputChannel.Receive();
+                Console.WriteLine("接受到一条消息，action为：{0}，body为：{1}", message.Headers.Action, message.GetBody<String>());
+                message.Close();
+                inputChannel.Close();
+                listener.Close();
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Datagram/Output/Output.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml;
+using System.Runtime.Serialization;
+namespace WCF.Second
+{
+    class Output
+    {
+        //入口方法
+        static void Main(string[] args)
+        {
+            try
+            {
+                //建立自定义的通道栈
+                BindingElement[] bindingElements = new BindingElement[3];
+                bindingElements[0] = new TextMessageEncodingBindingElement();
+                //OneWayBindingElement可以使得传输通道支持数据报模式
+                bindingElements[1] = new OneWayBindingElement();
+                bindingElements[2] = new HttpTransportBindingElement();
+                CustomBinding binding = new CustomBinding(bindingElements);
+                using (Message message = Message.CreateMessage(binding.MessageVersion, "sendMessage","Message Body"))
+                {
+                    //创建ChannelFactory
+                    IChannelFactory<IOutputChannel> factory = binding.BuildChannelFactory<IOutputChannel>(new BindingParameterCollection());
+                    factory.Open();
+                    //这里创建IOutputChannel
+                    IOutputChannel outputChannel = factory.CreateChannel(new EndpointAddress("http://localhost/InputService"));
+                    outputChannel.Open();
+                    //发送消息
+                    outputChannel.Send(message);
+                    Console.WriteLine("已经成功发送消息！");
+                    outputChannel.Close();
+                    factory.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
