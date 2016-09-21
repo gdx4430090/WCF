a993cf44f8a523a49052f1c055da641404ba75d6
高东霞 <dongxia.gao@dazong.com>
2016-09-21 17:48:52 +0800

    2、请求-响应模式C-S通信

---
+++ Request-Reply/Reply/Reply.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+namespace WCF.Second
+{
+    class Input
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //建立和发送端相同的通道栈
+                BindingElement[] bindingElements = new BindingElement[2];
+                bindingElements[0] = new TextMessageEncodingBindingElement();
+                bindingElements[1] = new HttpTransportBindingElement();
+                CustomBinding binding = new CustomBinding(bindingElements);
+                //建立ChannelListner
+                IChannelListener<IReplyChannel> listener = binding.BuildChannelListener<IReplyChannel>(new Uri("http://localhost/RequestReplyService"), new BindingParameterCollection());
+                listener.Open();
+                //创建IReplyChannel
+                IReplyChannel replyChannel = listener.AcceptChannel();
+                replyChannel.Open();
+                Console.WriteLine("开始接受消息。。。");
+                //接受并打印消息
+                RequestContext requestContext = replyChannel.ReceiveRequest();
+                Console.WriteLine("接受到一条消息，action为：{0}，body为：{1}", requestContext.RequestMessage.Headers.Action, requestContext.RequestMessage.GetBody<String>());
+                //创建返回消息
+                Message response = Message.CreateMessage(binding.MessageVersion, "response", "reponse body");
+                //发送返回消息
+                requestContext.Reply(response);
+                //关闭
+                requestContext.Close();
+                replyChannel.Close();
+                listener.Close();
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Request-Reply/Request/Request.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml;
+using System.Runtime.Serialization;
+namespace WCF.Second
+{
+    class Output
+    {
+        //入口方法
+        static void Main(string[] args)
+        {
+            try
+            {
+                //建立自定义的通道栈
+                BindingElement[] bindingElements = new BindingElement[2];
+                bindingElements[0] = new TextMessageEncodingBindingElement();
+                bindingElements[1] = new HttpTransportBindingElement();
+                CustomBinding binding = new CustomBinding(bindingElements);
+                using (Message message = Message.CreateMessage(binding.MessageVersion, "sendMessage","Message Body"))
+                {
+                    //创建ChannelFactory
+                    IChannelFactory<IRequestChannel> factory = binding.BuildChannelFactory<IRequestChannel>(new BindingParameterCollection());
+                    factory.Open();
+                    //这里创建IRequestChannel
+                    IRequestChannel requestChannel = factory.CreateChannel(new EndpointAddress("http://localhost/RequestReplyService"));
+                    requestChannel.Open();
+                    //发送消息
+                    Message response = requestChannel.Request(message);
+                    Console.WriteLine("已经成功发送消息！");
+                    //查看返回消息
+                    Console.WriteLine("接受到一条返回消息，action为：{0}，body为：{1}", response.Headers.Action, response.GetBody<String>());
+                    requestChannel.Close();
+                    factory.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
