3d05c52e1fbf01460f9b1256494eb885ee26fd2a
高东霞 <dongxia.gao@dazong.com>
2016-09-23 11:20:18 +0800

    3、双工模式
    Notes: 有错误，不能运行。后续修改

---
+++ Duplex/DuplexSender/DuplexSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+namespace WCF.Second
+{
+    class DuplexSender
+    {
+        //入口方法
+        static void Main(string[] args)
+        {
+            try
+            {
+                NetTcpBinding binding = new NetTcpBinding();
+                using (Message message = Message.CreateMessage(binding.MessageVersion, "sendMessage", "Message Body"))
+                {
+                    //创建ChannelFactory
+                    IChannelFactory<IDuplexChannel> factory = binding.BuildChannelFactory<IDuplexChannel>(new BindingParameterCollection());
+                    factory.Open();
+                    //这里创建IRequestChannel
+                    IDuplexChannel duplexChannel = factory.CreateChannel(new EndpointAddress("net.tcp://localhost:9090/DuplexService/Point2"));
+                    duplexChannel.Open();
+                    //发送消息
+                    duplexChannel.Send(message);
+                    Console.WriteLine("已经成功发送消息！");
+                    //var msg = duplexChannel.Receive();
+                    //Console.WriteLine($"收到消息：{msg.GetBody<string>()}");
+                    duplexChannel.Close();
+                    factory.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
